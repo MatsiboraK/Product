71c4df8b7fdd9952ed58ec911df4915eaa0ea0d5
MatsiboraK <kmacibora50@gmail.com>
2023-04-27 14:43:48 +0700

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -0,0 +1,17 @@
+package ru.netology.javaqa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product book = new Book(
+                1,
+                "Моя весна",
+                200,
+                "Александр Шубкин",
+                100,
+                1947
+        );
+
+        book.use();
+    }
+}
